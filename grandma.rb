f7fa43c3f2a0aa013b8268db89c165b90ff5a92d
Justin Wang <justinw827@aol.com>
2018-07-15 02:45:51 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -16,11 +16,10 @@ def speak_to_grandma(phrase)
   phrase = ""
   if phrase == "I LOVE YOU GRANDMA!"
     phrase = "I LOVE YOU TOO PUMPKIN!"
-  elsif  phrase == phrase.upcase
-      phrase = "NO, NOT SINCE 1938!"
-    else 
-      phrase = "HUH?! SPEAK UP, SONNY!"
-    end
+  elsif phrase == phrase.upcase && phrase != "I LOVE YOU GRANDMA!"
+    phrase = "NO, NOT SINCE 1938!"
+  else
+    phrase = "HUH?! SPEAK UP, SONNY!"
   end
   return phrase
 end
